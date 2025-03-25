11badbf0972db568b792b7d4557400b270e09084
Nikola Klacar <63237081+CarklaN@users.noreply.github.com>
2025-03-25 23:07:21 +0100

    Update CompanyPage.jsx

--- Section2_JobBoardProject/client/src/pages/CompanyPage.jsx
+++ Section2_JobBoardProject/client/src/pages/CompanyPage.jsx
@@ -5,15 +5,33 @@ import { getCompany } from '../lib/graphql/queries';
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const [company, setCompany] = useState();
+  const [state, setState] = useState({
+    company: null,
+    loading: null,
+    error: false,
+  });
   useEffect(() => {
-    getCompany(companyId).then(setCompany);
-  }, [companyId]);
+    (async () => {
+      try {
+        const company = await getCompany(companyId);
+        setState({ company, loading: false, error: false });
+      } catch (error){
+        console.log('Error: ', JSON.stringify(error, null, 2));
+        setState({ company: null, loading: false, error: true });
+      }
 
-  console.log('[CompanyPage] company:', company);
-  if (!company) {
+    })();
+  }, [companyId, error]);
+
+  console.log('[CompanyPage] company:', state);
+  const {company, loading, error} = state;
+  if (loading) {
     return <div>Loading...</div>;
   }
+  if (error) {
+    return <div className='has-text-danger'>Data unvailable...</div>;
+  }
+
   return (
     <div>
       <h1 className="title">
