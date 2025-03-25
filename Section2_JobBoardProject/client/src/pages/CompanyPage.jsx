54024079296edbde13a15cf16dd36e5916abebc1
Nikola Klacar <63237081+CarklaN@users.noreply.github.com>
2025-03-25 23:07:53 +0100

    Update CompanyPage.jsx

--- Section2_JobBoardProject/client/src/pages/CompanyPage.jsx
+++ Section2_JobBoardProject/client/src/pages/CompanyPage.jsx
@@ -7,7 +7,7 @@ function CompanyPage() {
   const { companyId } = useParams();
   const [state, setState] = useState({
     company: null,
-    loading: null,
+    loading: true,
     error: false,
   });
   useEffect(() => {
@@ -15,23 +15,21 @@ function CompanyPage() {
       try {
         const company = await getCompany(companyId);
         setState({ company, loading: false, error: false });
-      } catch (error){
-        console.log('Error: ', JSON.stringify(error, null, 2));
+      } catch (error) {
+        console.log('error:', JSON.stringify(error, null, 2));
         setState({ company: null, loading: false, error: true });
       }
-
     })();
-  }, [companyId, error]);
+  }, [companyId]);
 
-  console.log('[CompanyPage] company:', state);
-  const {company, loading, error} = state;
+  console.log('[CompanyPage] state:', state);
+  const { company, loading, error } = state;
   if (loading) {
     return <div>Loading...</div>;
   }
   if (error) {
-    return <div className='has-text-danger'>Data unvailable...</div>;
+    return <div className="has-text-danger">Data unavailable</div>;
   }
-
   return (
     <div>
       <h1 className="title">
@@ -48,4 +46,4 @@ function CompanyPage() {
   );
 }
 
-export default CompanyPage
+export default CompanyPage;
