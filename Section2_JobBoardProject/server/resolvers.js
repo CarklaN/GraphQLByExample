c4a8d3e63381be7fe6b4c1652fe30c6d4e590d88
Nikola Klacar <63237081+CarklaN@users.noreply.github.com>
2025-03-25 23:07:15 +0100

    resolver fix

--- Section2_JobBoardProject/server/resolvers.js
+++ Section2_JobBoardProject/server/resolvers.js
@@ -1,11 +1,24 @@
+import { GraphQLError } from 'graphql';
 import { getCompany } from './db/companies.js';
 import { getJob, getJobs, getJobsByCompany } from './db/jobs.js';
 
 export const resolvers = {
   Query: {
-    company: (_root, { id }) => getCompany(id),
-    job: (_root, { id }) => getJob(id),
-    jobs: () => getJobs(),
+    company: async (_root, { id }) => {
+        const company = await getCompany(id);
+        if(!company) {
+            throw notFoundError("No Company found with id"+id)
+        }
+        return company;
+    },
+    job: async (_root, { id }) => {
+       const job = await getJob(id)
+       if(!job) {
+        throw notFoundError("No job found for if: "+id)
+       }
+       return job
+    },
+    jobs: () => getJobs()
   },
 
   Company: {
@@ -17,6 +30,12 @@ export const resolvers = {
     date: (job) => toIsoDate(job.createdAt),
   },
 };
+
+function notFoundError(message) {
+    throw new GraphQLError("No Company found with id " + id, {
+        extensions: { code: 'NOT_FOUND'},
+    });
+}
 
 function toIsoDate(value) {
   return value.slice(0, 'yyyy-mm-dd'.length); // only to displaye year, month and day
